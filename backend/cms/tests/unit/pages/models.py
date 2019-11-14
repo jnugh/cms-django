b956765ac25900c09d5da2a0ef4d8259edc9d5e2
Sascha Beele <sascha@beele.io>
2019-11-14 15:59:40 +0100

    Make sample test case parse pylint

--- backend/cms/tests/unit/pages/models.py
+++ backend/cms/tests/unit/pages/models.py
@@ -1,10 +1,11 @@
+from random import randint
 from django.test import TestCase
 
 
 class PageTranslation(TestCase):
-    #def setUp(self):
-        # PageTranslation.objects.create(title="Generic title")
+    def setUp(self):
+        self.num = randint(0, 10)
 
     def test_is_outdated(self):
         """Outdated PageTranslation is correctly determined as outdated"""
-        self.assertTrue(True)
+        self.assertTrue(self.num > 5)
