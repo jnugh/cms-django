e85ddd6abd135e9834d2feda28681020b26eef35
Sascha Beele <sascha@beele.io>
2019-11-20 17:20:14 +0100

    First test cases for PageTranslation

--- backend/cms/tests/pages/models.py
+++ backend/cms/tests/pages/models.py
@@ -1,36 +1,44 @@
 from random import randint
 from django.test import TestCase
-from django.utils import timezone
 
-from cms.models import Page, PageTranslation
+from cms.models import Page, PageTranslation, Region
 
 
 class PageTest(TestCase):
     def setUp(self):
         # Setup run before every test method.
-        self.page = Page.objects.create(
-            parent=None,
-            icon=None,
-            region=None,
-            archived=False,
-            mirrored_page=None,
-            editors=None,
-            publishers=None,
-            created_date=timezone.now()
+        self.region = Region.objects.create(
+            aliases=[],
+            push_notification_channels=[],
+            slug='testregion'
         )
+        self.page1 = Page.objects.create(region=self.region)
+        self.page2 = Page.objects.create(parent=self.page1, region=self.region)
+        self.page3 = Page.objects.create(parent=self.page2, region=self.region)
 
-    def depth_no_parent(self):
-        # Depth is correctly determined for page without parent
-        self.assertTrue(self.page.depth == 0)
+    def test_depth_no_parent(self):
+        # Depth is correctly determined for page on first level.
+        self.assertTrue(self.page1.depth == 0)
+
+    def test_depth_third_level(self):
+        # Depth is correctly determined for page on third level.
+        self.assertTrue(self.page3.depth == 2)
+
+    def test_get_archived(self):
+        self.page1.archived = True
+        self.page1.save()
+        self.page2.archived = True
+        self.page2.save()
+        archived = Page.get_archived(self.region.slug)
+        self.assertTrue(len(archived) == 2)
 
 
 class PageTranslationTest(TestCase):
     def setUp(self):
         # Setup run before every test method.
-        self.num = randint(0, 10)
-        #Page.objects.create()
-        #PageTranslation.objects.create()
-
-    def test_is_outdated(self):
-        # Outdated PageTranslation is correctly determined as outdated
-        self.assertTrue(self.num > -1)
+        self.region = Region.objects.create(
+            aliases=[],
+            push_notification_channels=[],
+            slug='testregion'
+        )
+        self.pageTranslation = PageTranslation.objects.create()
