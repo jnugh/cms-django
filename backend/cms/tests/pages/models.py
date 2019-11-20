5b33ef443625aee5a483db440dbaebebb3e55fa4
Sascha Beele <sascha@beele.io>
2019-11-20 15:20:08 +0100

    Add test case for depth property in Page class

--- backend/cms/tests/pages/models.py
+++ backend/cms/tests/pages/models.py
@@ -1,5 +1,6 @@
 from random import randint
 from django.test import TestCase
+from django.utils import timezone
 
 from cms.models import Page, PageTranslation
 
@@ -7,9 +8,20 @@ from cms.models import Page, PageTranslation
 class PageTest(TestCase):
     def setUp(self):
         # Setup run before every test method.
-        Page.objects.create(
-
+        self.page = Page.objects.create(
+            parent=None,
+            icon=None,
+            region=None,
+            archived=False,
+            mirrored_page=None,
+            editors=None,
+            publishers=None,
+            created_date=timezone.now()
         )
+
+    def depth_no_parent(self):
+        # Depth is correctly determined for page without parent
+        self.assertTrue(self.page.depth == 0)
 
 
 class PageTranslationTest(TestCase):
